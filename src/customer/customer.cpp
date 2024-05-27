0a84eadeaa2c31c011be9c070d6bd3150a79f171
mlewko1 <matilew99@gmail.com>
2024-05-27 15:42:23 +0200

    added fname, lname, birth_date, gender

--- src/customer/customer.cpp
+++ src/customer/customer.cpp
@@ -1,14 +1,42 @@
 #include "customer.h"
 
-Customer::Customer(std::string id, std::string name, std::string email, std::string contactNumber) 
-    : id(id), name(name), email(email), contactNumber(contactNumber) {}
+Customer::Customer(std::string id, std::string fname, std::string lname, std::tm birthDate, std::string gender, std::string email, std::string phoneNumber) 
+    : id(id), fname(fname), lname(lname), birthDate(birthDate), gender(gender), email(email), phoneNumber(phoneNumber) {}
 
-void Customer::updateContact(std::string contactNumber) {
-    this->contactNumber = contactNumber;
+void Customer::updatePhoneNumber(std::string new_number)
+{
+    phoneNumber = new_number;
 }
 
-std::string Customer::getId() {
+void Customer::updateEmail(std::string new_email)
+{
+    email = new_email;
+}
+
+std::string Customer::getId()
+{
     return this->id;
+}
+
+std::string Customer::getFname() {
+    return this->fname;
+}
+
+std::string Customer::getLname() {
+    return this->lname;
+}
+
+// Zwracamy datę w string, bo w formacie tm jest też czas
+std::string Customer::getBirthDate()
+{
+    char buffer[11];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d", &this->birthDate);
+    return std::string(buffer);
+}
+
+std::string Customer::getGender()
+{
+    return this->gender;
 }
 
 std::string Customer::getEmail()
@@ -16,10 +44,7 @@ std::string Customer::getEmail()
     return this->email;
 }
 
-std::string Customer::getName() {
-    return this->name;
-}
-
-std::string Customer::getContactNumber() {
-    return this->contactNumber;
+std::string Customer::getPhoneNumber()
+{
+    return this->phoneNumber;
 }
--- src/customer/customer.h
+++ src/customer/customer.h
@@ -1,21 +1,36 @@
 #pragma once
 
 #include <string>
+#include <ctime>
 
 class Customer
 {
 public:
-    Customer(std::string id, std::string name, std::string email, std::string contact);
-    void updateContact(std::string contact);
+    Customer(std::string id, std::string fname, std::string lname, std::tm birthDate, std::string gender, std::string email, std::string contact);
+    void updatePhoneNumber(std::string new_number);
+    void updateEmail(std::string new_email);
     std::string getId();
+    std::string getFname();
+    std::string getLname();
+    std::string getBirthDate();
+    std::string getGender();
     std::string getEmail();
-    std::string getName();
-    std::string getContactNumber();
+    std::string getPhoneNumber();
 
 private:
     std::string id;
-    std::string name;
+    std::string fname;
+    std::string lname;
+    std::tm birthDate;
+    std::string gender;
     std::string email;
-    std::string contactNumber;
+    std::string phoneNumber;
     // password ?????????????
 };
+
+
+// Tworzenie daty urodzenia:
+// std::tm birthDate = {};
+// birthDate.tm_year = year-1900; // years since 1900
+// birthDate.tm_mon = month-1; // months since January (0-11)
+// birthDate.tm_mday = day; // day of the month (1-31)
